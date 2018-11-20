e25485c282034a7e846c5f9226a331f74d9ea6d9
cesar <cesarolimpio@gmail.com>
2018-11-20 14:17:26 -0200

    body parser and cors handling

--- server/api/routes/encoder.js
+++ server/api/routes/encoder.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+  const video = {
+    name: req.body.name
+  };
   res.status(200).json({
-    message: 'Handling GET to /encoder'
+    message: 'Handling GET to /encoder',
+    encoded: video
   });
 });
 
--- server/app.js
+++ server/app.js
@@ -12,6 +12,16 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.use('/encode', encoderRoutes);
 
 app.use((req, res, next) => {
